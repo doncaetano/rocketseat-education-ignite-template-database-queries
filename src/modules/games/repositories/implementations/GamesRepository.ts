f55d65cf694f5d18d9159cd7be8411f65e102968
Rhuan Caetano <rhuancaetano@gmail.com>
2021-03-30 21:12:42 -0300

    done

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -14,17 +14,24 @@ export class GamesRepository implements IGamesRepository {
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder()
+    .createQueryBuilder('game')
+    .where("game.title ILike :title", { title: `%${param}%` })
+    .getMany();
       // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT COUNT(*) FROM games'); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
+    const game = await this.repository
+      .createQueryBuilder('game')
+      .innerJoinAndSelect('game.users', 'users')
+      .where('game.id = :id', { id })
+      .getOne();
+
+    return game?.users || [];
       // Complete usando query builder
   }
 }
--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -15,16 +15,19 @@ export class UsersRepository implements IUsersRepository {
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    return await this.repository.findOneOrFail(user_id, {
+      relations: ['games']
+    })
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query('SELECT * from users ORDER BY first_name ASC'); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query(`SELECT * FROM users WHERE first_name ILike '%${first_name}%' AND last_name ILike '%${last_name}%'`); // Complete usando raw query
   }
 }
